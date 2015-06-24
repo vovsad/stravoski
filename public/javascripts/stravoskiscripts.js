4cf4f2d5394bb2338bf543d170af16b5e6e3c575
Volodymyr <vsadovyy@yahoo.com>
2015-06-24 16:28:53 +0300

    activities reload with js added

--- public/javascripts/stravoskiscripts.js
+++ public/javascripts/stravoskiscripts.js
@@ -4,82 +4,87 @@
 
 var app = angular.module('StravoSki', ['ui.bootstrap']);
 
-app.controller("AthleteCtrl", function($scope, $http) {
-	  $http.get('/getathletestat').
+app.controller("TopCtrl", function($scope, $http, $modal, $log) {
+
+	$scope.loadAthleteStatistics = function () {
+		  $http.get('/getathletestat').
+		    success(function(data, status, headers, config) {
+		      $scope.statistics = data;
+		    }).
+		    error(function(data, status, headers, config) {
+		    	$scope.message = "Something goes wrong";
+		    });
+	};
+	$scope.loadAthleteStatistics();
+
+	$scope.loadActivities = function () {
+	  $http.get('/getactivities').
 	    success(function(data, status, headers, config) {
-	      $scope.statistics = data;
+	      $scope.activities = data;
 	    }).
 	    error(function(data, status, headers, config) {
 	    	$scope.message = "Something goes wrong";
 	    });
-	});
+	};
+	$scope.loadActivities();
 
 
-app.controller("ActivitiesCtrl", function($scope, $http) {
-  $http.get('/getactivities').
-    success(function(data, status, headers, config) {
-      $scope.activities = data;
-    }).
-    error(function(data, status, headers, config) {
-    	$scope.message = "Something goes wrong";
-    });
-});
 
-app.controller("TopCtrl", function($scope, $http, $modal, $log) {
+
 	  
-	  $scope.modalDialog = function(title, body, image) {
-
+	$scope.modalDialog = function(title, body, image) {
+	
 			var modalInstance = $modal.open({
 			      animation: $scope.animationsEnabled,
 			      templateUrl: 'modalContent.html',
-			      controller: 'ModalInstanceCtrl',
-			      resolve: {
-			        messageBody: function () {
-			        	return body;
-			        },
-			        messageTitle: function () {
-			        	return title;
-			        },
-			        messageImage: function () {
-			        	return image;
-			        }
-			      }
-			    });
-
-			    modalInstance.result.then(function (selectedItem) {
-			      $scope.selected = selectedItem;
-			    }, function () {
-			      $log.info('Modal dismissed at: ' + new Date());
-			    });
-		};
+		      controller: 'ModalInstanceCtrl',
+		      resolve: {
+		        messageBody: function () {
+		        	return body;
+		        },
+		        messageTitle: function () {
+		        	return title;
+		        },
+		        messageImage: function () {
+		        	return image;
+		        }
+		      }
+		    });
+	
+		    modalInstance.result.then(function (selectedItem) {
+		      $scope.selected = selectedItem;
+		    }, function () {
+		      $log.info('Modal dismissed at: ' + new Date());
+		    });
+	};
 		
-	  $scope.modalDialogActivityDetails = function (a){
-		  var mapURL = 'http://maps.googleapis.com/maps/api/staticmap?sensor=false&size=150x150&path=weight:3|color:red|enc:';
-		  mapURL += a.map.summary_polyline;
-		  var details = 'Skiied ' + Math.round(a.distance/1000) + ' km ';
-		  if(a.location_city != null){
-		  	details += 'at ' + a.location_city;
-		  }else if(a.location_state != null){
-			  details += 'at ' + a.location_state;
-		  }
-		  details += ' for ' + Math.floor(a.moving_time/3600) 
+	$scope.modalDialogActivityDetails = function (a){
+		var mapURL = 'http://maps.googleapis.com/maps/api/staticmap?sensor=false&size=150x150&path=weight:3|color:red|enc:';
+		mapURL += a.map.summary_polyline;
+		var details = 'Skiied ' + Math.round(a.distance/1000) + ' km ';
+		if(a.location_city != null){
+			details += 'at ' + a.location_city;
+		}else if(a.location_state != null){
+			details += 'at ' + a.location_state;
+		}
+		details += ' for ' + Math.floor(a.moving_time/3600) 
 		  				+ ':' + (Math.floor(a.moving_time/60) - Math.floor(a.moving_time/3600)*60) + ' moving time.';
 		  
-		  $scope.modalDialog(a.name, details, mapURL);
+		$scope.modalDialog(a.name, details, mapURL);
 	  };
 	
-	  $scope.doSyncWithStrava = function () {
-		  
-	      	$http.get('/syncwithstrava').
-		    success(function(data, status, headers, config) {
-		    	$scope.modalDialog('Notification', 
-		    			'Your Activities are just synced and cached');
-		    }).
-		    error(function(data, status, headers, config) {
-		    	$scope.modalDialog('Error', 
-    			'Something goes wrong');
-		    });
-		  };
+	$scope.doSyncWithStrava = function () {
+      	$http.get('/syncwithstrava').
+	    success(function(data, status, headers, config) {
+	    	$scope.modalDialog('Notification', 
+	    			'Your Activities are just synced and cached');
+	    	$scope.loadActivities();
+	    }).
+	    error(function(data, status, headers, config) {
+	    	$scope.modalDialog('Error', 
+			'Something goes wrong');
+	    });
+	};
 
 });
 
@@ -90,5 +95,5 @@ app.controller('ModalInstanceCtrl', function ($scope, $modalInstance, messageBod
 	  $scope.ok = function () {
 	    $modalInstance.close('ok');
 	  };
-	});
+});
 
